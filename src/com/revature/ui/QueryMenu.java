fc3ed7c8d56bbef3645bb93c0062979be1b354d6
Nate Ford <nathaniel.ford@revature.net>
2021-02-24 10:14:00 -0600

    Added loop logic to QueryMenue

--- src/com/revature/ui/QueryMenu.java
+++ src/com/revature/ui/QueryMenu.java
@@ -3,6 +3,7 @@ package com.revature.ui;
 import java.util.Scanner;
 
 public abstract class QueryMenu {
+    //"y" returns true, "n" returns false
     public static boolean yesNo(Scanner scan){
         boolean result = false;
         boolean continueLoop = true;
@@ -14,6 +15,31 @@ public abstract class QueryMenu {
                 result = true;
             } else if(answer.equals("n")){
                 continueLoop = false;
+            }
+        } while(continueLoop);
+        return result;
+    }
+
+    //return String of the selected query
+    public static String query(Scanner scan, String[] queries){
+        String result = "";
+        boolean continueLoop = true;
+        do {
+        //display all query options
+        for(int i = 0; i<queries.length; i++){
+            System.out.print(queries[i]);
+            //add slash after all but the last query
+            if (i<queries.length-1){System.out.print("/");}
+        }
+            System.out.println();
+            //check which option was chosen
+            String answer = scan.nextLine();
+            for (String q : queries) {
+                if (answer.equals(q)) {
+                    result = q;
+                    continueLoop = false;
+                    break;
+                }
             }
         } while(continueLoop);
         return result;
