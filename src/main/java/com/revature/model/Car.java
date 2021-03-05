82c3f2c11d91f08876dab2a80bc3c87c37a581cd
Nate Ford <nathaniel.ford@revature.net>
2021-03-05 13:08:03 -0600

    basic Lot methods

--- src/main/java/com/revature/model/Car.java
+++ src/main/java/com/revature/model/Car.java
@@ -1,4 +1,17 @@
 package com.revature.model;
 
 public class Car {
+    private String name;
+
+    public Car(String n){
+        name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
 }
--- src/main/java/com/revature/model/Lot.java
+++ src/main/java/com/revature/model/Lot.java
@@ -1,4 +1,19 @@
 package com.revature.model;
 
+import com.revature.collections.FlexArray;
+
 public abstract class Lot {
+    private FlexArray<Car> cars = new FlexArray<Car>();
+
+    public void addCar(Car c){
+        cars.add(c);
+    }
+
+    public void addCar(Car[] c){
+        cars.add(c);
+    }
+
+    public Car[] getCars(){
+        return (Car[]) cars.toArray();
+    }
 }
