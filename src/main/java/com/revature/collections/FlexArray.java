2c3dd3cbc7041b6e91bd0db71d570d1b53d04860
Nate Ford <nathaniel.ford@revature.net>
2021-03-09 12:18:09 -0600

    Fully Generified FlexArray (Finally)

--- src/main/java/com/revature/collections/FlexArray.java
+++ src/main/java/com/revature/collections/FlexArray.java
@@ -1,51 +1,59 @@
 package com.revature.collections;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+// generic array from: https://www.baeldung.com/java-generic-array
+
 // Object that contains an array
 // makes a new array with a larger size each time an element is added
-public class FlexArray<T>{
+public class FlexArray<E>{
 
-    protected Object[] array;
+    protected E[] elements;
+    protected Class<?> clazz;
 
-    public FlexArray(){
-        array = new Object[0];
+    public FlexArray(Class<?> clazz){
+        this.clazz = clazz;
+        elements = array(0);
     }
-    public FlexArray(T[] array){
-        this.array = array;
+
+    private E[] array(int length){
+        return (E[]) Array.newInstance(clazz, length);
     }
 
-    public void add(T t) {
-        Object[] newArray = new Object[array.length + 1];
-        System.arraycopy(array, 0, newArray, 0, array.length);
+    public void add(E e) {
+        E[] newArray = array(elements.length + 1);
+        System.arraycopy(elements, 0, newArray, 0, elements.length);
         // add new element
-        newArray[array.length] = t;
-        array = newArray;
+        newArray[elements.length] = e;
+        elements = newArray;
     }
 
-    public void add(T[] newElements) {
-        Object[] newArray = new Object[array.length + newElements.length];
-        System.arraycopy(array, 0, newArray, 0, array.length);
-        System.arraycopy(newElements, 0, newArray, array.length, newElements.length);
+    public void add(E[] newElements) {
+        E[] newArray = array(elements.length + newElements.length);;
+        System.arraycopy(elements, 0, newArray, 0, elements.length);
+        System.arraycopy(newElements, 0, newArray, elements.length, newElements.length);
         // add new elements
-        array = newArray;
+        elements = newArray;
     }
 
     // removes any element of the internal array that matches o by making a new array of reduced size without those elements
-    public void remove(T t) {
+    public void remove(E e) {
         int removalIndex = -1;
         // number of items removed -1
         int removalCount = -1;
         boolean loop = true;
         boolean remove;
-        Object[] prevArray;
-        Object[] result = array;
+        E[] prevArray;
+        E[] result = elements;
         do {
             remove = false;
-            for (int i = removalIndex+1; i < array.length; i++) {
+            for (int i = removalIndex+1; i < elements.length; i++) {
                 // check if this is last loop
-                if (i == array.length-1) {
+                if (i == elements.length-1) {
                     loop = false;
                 }
-                if (array[i].equals(t)) {
+                if (elements[i].equals(e)) {
                     removalIndex = i;
                     removalCount++;
                     remove = true;
@@ -55,28 +63,28 @@ public class FlexArray<T>{
             if(remove){
                 // result will be one less in length for each removal we find
                 prevArray = result;
-                result = new Object[result.length-1];
+                result = array(result.length-1);
                 // prevArray will have length of (array - removalCount + 1)
                 System.arraycopy(prevArray, 0, result, 0, removalIndex-removalCount);
                 System.arraycopy(prevArray, removalIndex-removalCount+1, result, removalIndex-removalCount, result.length-(removalIndex-removalCount));
             }
         } while(loop);
-        array = result;
+        elements = result;
     }
 
     // replaces array with an array without the element at index i an with length -1
     // should throw an exception if the index given is out of scope
     public void remove(int i) {
-        Object[] result = new Object[array.length-1];
-        System.arraycopy(array, 0, result, 0, i);
-        System.arraycopy(array, i+1, result, i, array.length-i-1);
-        array = result;
+        E[] result = array(elements.length-1);
+        System.arraycopy(elements, 0, result, 0, i);
+        System.arraycopy(elements, i+1, result, i, elements.length-i-1);
+        elements = result;
     }
 
-    public Object get(int index){ return array[index]; }
+    public E get(int index){ return elements[index]; }
 
     public boolean isEmpty() {
-        for (Object t : array) {
+        for (Object t : elements) {
             if (t != null) {
                 return false;
             }
@@ -86,21 +94,21 @@ public class FlexArray<T>{
 
     // Bubble Sort
     public void order() {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i].toString().compareTo(array[j].toString()) > 0) {
-                    Object temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
+        for (int i = 0; i < elements.length; i++) {
+            for (int j = i + 1; j < elements.length; j++) {
+                if (elements[i].toString().compareTo(elements[j].toString()) > 0) {
+                    E temp = elements[i];
+                    elements[i] = elements[j];
+                    elements[j] = temp;
                 }
             }
         }
     }
 
     //Returns index of user in array or -1 if user isn't found
-    public int indexOf(T u) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i].equals(u)) {
+    public int indexOf(E u) {
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i].equals(u)) {
                 return i;
             }
         }
@@ -108,13 +116,13 @@ public class FlexArray<T>{
     }
 
     public int size(){
-        return array.length;
+        return elements.length;
     }
 
     public String[] toStringArray(){
-        String[] result = new String[array.length];
-        for (int i = 0; i < array.length; i++){
-            result[i] = array[i].toString();
+        String[] result = new String[elements.length];
+        for (int i = 0; i < elements.length; i++){
+            result[i] = elements[i].toString();
         }
         return result;
     }
@@ -128,7 +136,7 @@ public class FlexArray<T>{
         return result;
     }
 
-    public Object[] toArray() {
-        return array;
+    public E[] toArray() {
+        return Arrays.copyOf(elements, elements.length);
     }
 }
